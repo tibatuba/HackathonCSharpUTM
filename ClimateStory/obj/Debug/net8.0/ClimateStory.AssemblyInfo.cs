1ddd2e05836d2584ccc4da43f2ce01a311cfe4f1
Neha Desai <desaineh@shernet.sheridancollege.ca>
2024-10-05 16:07:21 -0400

    merge

--- ClimateStory/obj/Debug/net8.0/ClimateStory.AssemblyInfo.cs
+++ ClimateStory/obj/Debug/net8.0/ClimateStory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClimateStory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1bea188d3c4df627fff9e44c995ce6ddf4ece40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b25f7c40d605655f01befcb686a7f8242e419f9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClimateStory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClimateStory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
