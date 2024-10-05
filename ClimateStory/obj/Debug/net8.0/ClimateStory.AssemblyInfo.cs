4560797d852766ff0174cfe74bf53bd70d351eb2
tibatuba <tibaaljaqobi@hotmail.com>
2024-10-05 17:34:39 -0400

    y

--- ClimateStory/obj/Debug/net8.0/ClimateStory.AssemblyInfo.cs
+++ ClimateStory/obj/Debug/net8.0/ClimateStory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClimateStory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abc19694dc4c0017f5e795088eb5e703d4c445af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59709cef4f86dce552691665c3d1164930a0382b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClimateStory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClimateStory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
